c9207239ff140abfc931f65b79012c2cd8947b7c
Dmitry Shelamov <dmitryshelamov@gmail.com>
2018-01-08 22:33:06 +0700

    Restructure project
    * Rename MainWindow to SettingsWindow
    * Change Settings window titel to "Settings"
    * Put SettingsWindow under View folder

---
+++ Snipping-Tool-Solution/SnippingTool/View/SettingsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SnippingTool.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
