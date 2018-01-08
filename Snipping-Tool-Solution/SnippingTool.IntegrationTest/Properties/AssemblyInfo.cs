3ad0da5456286a4567b95897ab0f9fe90b178121
Dmitry Shelamov <dmitryshelamov@gmail.com>
2018-01-08 17:22:37 +0700

    Add integration test project
    * Add nunit3 package

---
+++ Snipping-Tool-Solution/SnippingTool.IntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnippingTool.IntegrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnippingTool.IntegrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c940742e-ac56-44c0-88b1-350d9fa2c55f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
