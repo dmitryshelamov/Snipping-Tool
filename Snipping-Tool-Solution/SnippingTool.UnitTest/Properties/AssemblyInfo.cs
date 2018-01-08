85b8c3f513246a7d11f9d254e9b798588c8e59c6
Dmitry Shelamov <dmitryshelamov@gmail.com>
2018-01-08 17:17:26 +0700

    Add unit test project
    * Add nunit3 package
    * Add Nsubstitute

---
+++ Snipping-Tool-Solution/SnippingTool.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnippingTool.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnippingTool.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75d4d2d6-c164-41a6-afdc-96da57b8c02b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
