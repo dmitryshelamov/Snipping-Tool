308624737e3fc8e38971f7198c0623e62a691696
Dmitry Shelamov <dmitryshelamov@gmail.com>
2018-01-08 21:36:09 +0700

    Implement ISettingsRepository
    * Implement ISettingsRepository interface as SettingsRepository class
    * Add intehration test for SettingsRepository class

---
+++ Snipping-Tool-Solution/SnippingTool.IntegrationTest/SettingsRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using NUnit.Framework;
+using SnippingTool.Models;
+
+namespace SnippingTool.IntegrationTest
+{
+    [TestFixture]
+    class SettingsRepositoryTest
+    {
+        [Test]
+        public void Save_SaveUserSettingsToFile_PropertiesSaved()
+        {
+            //  arrange
+            string fileName = "UserSettings.xml";
+            string pathToConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            UserSettings settings = new UserSettings()
+            {
+                SaveDirectory = "TestSaveDirectory",
+                ImageExtentions = "TestExtenstions"
+            };
+
+            //  act
+            SettingsRepository settingsRepository = new SettingsRepository();
+            settingsRepository.Save(settings);
+
+            XDocument xDoc = XDocument.Load(pathToConfigFile);
+            UserSettings expectedSettings = new UserSettings()
+            {
+                SaveDirectory = xDoc.Element("UserSettings").Element("SaveDirectory").Value,
+                ImageExtentions = xDoc.Element("UserSettings").Element("ImageExtentions").Value
+            };
+            //  assert
+            Assert.IsTrue(File.Exists(pathToConfigFile));
+            Assert.AreEqual(expectedSettings.SaveDirectory, settings.SaveDirectory);
+            Assert.AreEqual(expectedSettings.ImageExtentions, settings.ImageExtentions);
+        }
+
+
+        [Test]
+        public void Load_SaveDirectoryProperty_PropertyLoaded()
+        {
+            //  arrange
+            var expectedSaveDirectory = "ExpectedSaveDirectory";
+            var expectedImageExtentions = "ExpectedImageExtentions";
+            var xml =
+                "<?xml version=\"1.0\"?>\n<UserSettings xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n" +
+                "<SaveDirectory>" + expectedSaveDirectory + "</SaveDirectory>\n" +
+                "<ImageExtentions>" + expectedImageExtentions + "</ImageExtentions>\n</UserSettings>";
+            string fileName = "UserSettings.xml";
+            string pathToConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            File.WriteAllText(pathToConfigFile, xml);
+            //  act
+            SettingsRepository settingsRepository = new SettingsRepository();
+            UserSettings userSettings = settingsRepository.Load();
+            //  assert
+            Assert.AreEqual(expectedSaveDirectory, userSettings.SaveDirectory);
+            Assert.AreEqual(expectedImageExtentions, userSettings.ImageExtentions);
+        }
+    }
+}
---
+++ Snipping-Tool-Solution/SnippingTool/Models/SettingsRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using SnippingTool.Models.Interfaces;
+
+namespace SnippingTool.Models
+{
+    public class SettingsRepository : ISettingsRepository
+    {
+        private readonly string _pathToConfigFile;
+        private const string ConfigFile = "UserSettings.xml";
+
+        public SettingsRepository()
+        {
+            _pathToConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFile);
+        }
+
+        public void Save(UserSettings userSettings)
+        {
+            using (FileStream fileStream = new FileStream(_pathToConfigFile, FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(UserSettings));
+                serializer.Serialize(fileStream, userSettings);
+            }
+        }
+
+        public UserSettings Load()
+        {
+            UserSettings userSettings;
+            using (FileStream fileStream = new FileStream(_pathToConfigFile, FileMode.Open))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(UserSettings));
+                userSettings = deserializer.Deserialize(fileStream) as UserSettings;
+            }
+            return userSettings;
+        }
+    }
+}
